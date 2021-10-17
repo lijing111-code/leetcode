0164e504c294990ae47335939b5e68d3b52e484e
Li Jing <lijing111@mail.ustc.edu.cn>
2021-10-17 17:03:40 +0800

    快排finish

--- 快排/kth_number/kth_number.go
+++ 快排/kth_number/kth_number.go
@@ -1,8 +1,42 @@
-package kth_number
+package main
 
-func main(){
+import "fmt"
+
+func main() {
+	var n, k int
+	fmt.Scanf("%d%d", &n, &k)
+	q := make([]int, n)
+	for i := 0; i < n; i++ {
+		fmt.Scanf("%d", &q[i])
+	}
+	fmt.Print(get_kth_numeber(0, n-1, k-1, q))
 }
 
-func get_kth_numeber(){
-
+func get_kth_numeber(l, r, k int, q []int) int {
+	if l == r {
+		return q[l]
+	}
+	x := q[(l+r)>>1]
+	i, j := l-1, r+1
+	for i < j {
+		for {
+			i++
+			if q[i] >= x {
+				break
+			}
+		}
+		for {
+			j--
+			if q[j] <= x {
+				break
+			}
+		}
+		if i < j {
+			q[i], q[j] = q[j], q[i]
+		}
+	}
+	if k<=j{
+		return get_kth_numeber(l,j,k,q)
+	}
+	return get_kth_numeber(j+1, r, k, q)
 }
--- 快排/quickSort/quickSort.go
+++ 快排/quickSort/quickSort.go
@@ -1,9 +1,41 @@
-package quickSort
+package main
 
-func main(){
-	quickSort(0,nil)
+import "fmt"
+
+func main() {
+	var n int
+	fmt.Scanf("%d",&n)
+	arr :=make([]int,n)
+	for i:=0;i<n;i++{
+		fmt.Scanf("%d",&arr[i])
+	}
+	quickSort(0,n-1,arr)
+	fmt.Println(arr)
 }
-func quickSort(n int,arr []int){
 
+func quickSort(l ,r int, arr []int){
+	if l>=r{// 起始条件
+		return
+	}
+	x :=arr[l]
+	i,j:=l-1,r+1// 两个指针，因为do while要先自增/自减
+	for i<j{
+		for{
+			i++
+			if arr[i]>=x{
+				break
+			}
+		}
+		for{
+			j--
+			if arr[j]<=x{
+				break
+			}
+		}
+		if i<j {
+			arr[i], arr[j] = arr[j], arr[i]
+		}
+	}
+	quickSort(l,j,arr)// 递归处理左右两段
+	quickSort(j+1,r,arr)
 }
-
