b53447fbc7ae87de48fa9abdf59ca5fc54113d5b
Li Jing <lijing111@mail.ustc.edu.cn>
2021-10-17 14:24:17 +0800

    start study

---
+++ 快排/kth_number/kth_number.go
@@ -0,0 +1,8 @@
+package kth_number
+
+func main(){
+}
+
+func get_kth_numeber(){
+
+}
---
+++ 快排/quickSort/quickSort.go
@@ -0,0 +1,9 @@
+package quickSort
+
+func main(){
+	quickSort(0,nil)
+}
+func quickSort(n int,arr []int){
+
+}
+
